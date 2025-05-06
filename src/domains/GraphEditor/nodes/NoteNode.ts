0028557baf46f28fbc32db6157266982b36b54b2
Luis Mendez <luis.mendez@generic.de>
2025-05-06 18:51:26 +0200

    removed not needed functions

--- src/domains/GraphEditor/nodes/NoteNode.ts
+++ src/domains/GraphEditor/nodes/NoteNode.ts
@@ -1,21 +1,10 @@
-import { defineNode, NodeInterface, AbstractNode, TextareaInputInterface } from 'baklavajs'
+import { defineNode, AbstractNode, TextareaInputInterface } from 'baklavajs'
 
 export const NoteNode = defineNode({
   type: 'NoteNode',
   title: '📑Note',
   inputs: {
     text: () => new TextareaInputInterface('Note', '').setPort(false),
-  },
-  outputs: {
-    content: () => new NodeInterface<string>('Content', '').setHidden(true),
-  },
-  calculate(inputs) {
-    // prepare outputs
-    const outputs = {
-      content: inputs.text,
-    }
-
-    return outputs
   },
   onCreate() {
     const node = this as unknown as AbstractNode
